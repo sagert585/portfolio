433a2c18c9a5af51e3cd0c4e6c9d6d1f9c096e52
sagert585 <ssheets@unm.edu>
2017-02-09 11:27:04 -0700

    Update map_page.js

--- map_page.js
+++ map_page.js
@@ -1,8 +1,8 @@
 function initialize () {
-  var AINL = new google.maps.LatLng(46.832547,-90.762879)
+  var islands = new google.maps.LatLng(46.832547,-90.762879)
   var mapOptions = {
     zoom: 12,
-    center: AINL,
+    center: islands,
     mapTypeID: google.maps.MapTypeId.TERRAIN
   }
   var map = new. google.maps.Map(
