45ee7443b6732becbcf393a383f603179e23d937
sagert585 <ssheets@unm.edu>
2017-02-09 10:38:28 -0700

    First attempt at a javascript map page.

---
+++ map_page.js
@@ -0,0 +1,11 @@
+function initialize () {
+  var AINL = new google.maps.LatLng(46.832547,-90.762879)
+  var mapOptions = {
+    zoom: 12,
+    center: AINL,
+    mapTypeID: google.maps.MapTypeId.TERRAIN
+  }
+  var map = new. google.maps.Map(
+    document.getElementById("map_canvas"),
+    mapOptions);
+}
