244cae330ba017e7f902de9f010f7e2150b05048
sagert585 <ssheets@unm.edu>
2017-03-21 15:22:51 -0600

    Update map_m8.js

--- js/map_m8.js
+++ js/map_m8.js
@@ -6,7 +6,7 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
+    center: ol.proj.fromLonLat([36.739857, 137.027963]),
     zoom: 4
   })
 });
