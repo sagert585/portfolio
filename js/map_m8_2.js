332aab53d74470c1bbfd88db8b2fc37257b8baf3
sagert585 <ssheets@unm.edu>
2017-03-29 16:04:58 -0600

    Create map_m8_2.js

---
+++ js/map_m8_2.js
@@ -0,0 +1,33 @@
+var Layer_Stamen_watercolor = new ol.layer.Group({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.Stamen({layer: 'watercolor'})
+        })
+    ]
+});
+
+var Layer_OSM = new ol.layer.Group({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ]
+});
+
+var map = new ol.Map({
+  target: 'map_canvas',
+  view: new ol.View({
+    center: ol.proj.fromLonLat([136.648869, 36.577930]), 
+    zoom: 15
+  })
+});
+
+function setMapType(newType) {
+    if(newType == 'OSM') {
+        map.setLayerGroup(Layer_OSM);
+    } else if (newType == 'STAMEN_Watercolor') {
+        map.setLayerGroup(Layer_Stamen_watercolor);
+    }
+}
+
+setMapType('STAMEN_Watercolor')
