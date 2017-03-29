5e793b7981831eff38a24dc88f883ebbada8226c
sagert585 <ssheets@unm.edu>
2017-03-29 16:18:04 -0600

    Update map_m8_2.js

--- js/map_m8_2.js
+++ js/map_m8_2.js
@@ -14,6 +14,17 @@ var Layer_OSM = new ol.layer.Group({
     ]
 });
 
+var layerCombo_1 = new ol.layer.Group({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.Stamen({layer: 'watercolor'})
+    }),
+    new ol.layer.Tile({
+      source: new ol.source.Stamen({layer: 'terrain-labels'})
+    })
+    ]
+});
+
 var map = new ol.Map({
   target: 'map_canvas',
   view: new ol.View({
@@ -27,7 +38,9 @@ function setMapType(newType) {
         map.setLayerGroup(Layer_OSM);
     } else if (newType == 'STAMEN_Watercolor') {
         map.setLayerGroup(Layer_Stamen_watercolor);
+    } else if (newType == 'Combo_1') {
+      map.setLayerGroup(layerCombo_1);
     }
 }
 
-setMapType('STAMEN_Watercolor')
+setMapType('Combo_1')
