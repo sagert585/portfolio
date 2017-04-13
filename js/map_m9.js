b0068f9a223c6426f785afdbf9e5d7e19b2bfcff
sagert585 <ssheets@unm.edu>
2017-04-12 19:06:14 -0600

    Create map_m9.js

---
+++ js/map_m9.js
@@ -0,0 +1,11 @@
+var projection = ol.proj.get('EPSG:3857');
+
+//for future reference, NM StatePlane Central EPSG is 2258, UTM 13 N is 26913
+
+var block_color = [0,255,0,.1]
+var block_line_color = [0,255,0,1]
+var county_color = [124,124,255,.25]
+var county_line_color = [124,124,255,1]
+
+///copying from Karl's example currently incomplete
+
