48cb53efbf8f8d97169a5ff405cffd4830539c0f
sagert585 <ssheets@unm.edu>
2017-05-05 13:14:03 -0600

    Update map_m9.js

--- js/map_m9.js
+++ js/map_m9.js
@@ -9,36 +9,28 @@ var county_line_color = [124,124,255,1]
 
 ///copying from Karl's example currently incomplete
 
-var Layer_Stamen_watercolor = new ol.layer.Group({
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.Stamen({layer: 'watercolor'})
+var sha_kml = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'https://sagert585.github.io/portfolio/kml/NM_SHA_qgis.kml',
+        projection: projection,
+        format: new ol.format.KML({
+            extractStyles:false
         })
-    ]
-});
+    }),
+    //style: SOMETHING
+})
 
-var Layer_OSM = new ol.layer.Group({
-    layers: [
-        new ol.layer.Tile({
+var Layer_OSM = new ol.layer.Tile({
             source: new ol.source.OSM()
-        })
-    ]
 });
 
 var map = new ol.Map({
   target: 'map_canvas',
+  layers: [
+      Layer_OSM, sha_kml
+      ],
   view: new ol.View({
     center: ol.proj.fromLonLat([-106.629361, 35.105380]),
-    zoom: 15
+    zoom: 8
   })
 });
-
-function setMapType(newType) {
-    if(newType == 'OSM') {
-        map.setLayerGroup(Layer_OSM);
-    } else if (newType == 'STAMEN_Watercolor') {
-        map.setLayerGroup(Layer_Stamen_watercolor);
-    }
-}
-
-setMapType('STAMEN_Watercolor')
