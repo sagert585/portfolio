c6f0b0b58bbccb449fc4e8bcc338f6216c592b89
sagert585 <ssheets@unm.edu>
2017-05-05 12:59:36 -0600

    Update map_m9.js

--- js/map_m9.js
+++ js/map_m9.js
@@ -9,3 +9,36 @@ var county_line_color = [124,124,255,1]
 
 ///copying from Karl's example currently incomplete
 
+var Layer_Stamen_watercolor = new ol.layer.Group({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.Stamen({layer: 'watercolor'})
+        })
+    ]
+});
+
+var Layer_OSM = new ol.layer.Group({
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ]
+});
+
+var map = new ol.Map({
+  target: 'map_canvas',
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-106.629361, 35.105380]),
+    zoom: 15
+  })
+});
+
+function setMapType(newType) {
+    if(newType == 'OSM') {
+        map.setLayerGroup(Layer_OSM);
+    } else if (newType == 'STAMEN_Watercolor') {
+        map.setLayerGroup(Layer_Stamen_watercolor);
+    }
+}
+
+setMapType('STAMEN_Watercolor')
