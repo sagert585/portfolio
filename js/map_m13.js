aba4febb349a93956ca7178ac119e900d3ff3419
sagert585 <ssheets@unm.edu>
2017-05-05 11:39:42 -0600

    Update map_m13.js

--- js/map_m13.js
+++ js/map_m13.js
@@ -1,6 +1,81 @@
-var projection = ol.proj.get('EPSG:26913')
+var projection = ol.proj.get('EPSG:26913');
 
 var SPAR = new ol.layer.Tile({
+  source: new ol.source.TileWMS({
+    attributions: new ol.Attribution({
+      html: 'ESRI Maps & Data'
+    }),
+    params: {'LAYERS':'ws_ssheets:e2sfcam_blockGroups'},
+    url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
+    serverType: 'geoserver',
+    projection: projection,
+    format: new ol.format.KML({
+      extractStyles:false
+    })
+  }),
+})
+
+var SHA = new ol.layer.Tile({
+  source: new ol.source.TileWMS({
+    attributions: new ol.Attribution({
+      html: 'ESRI Maps & Data'
+    }),
+    params: {'LAYERS':'ws_ssheets:e2sfcam_blockGroups'},
+    url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
+    serverType: 'geoserver',
+    projection: projection,
+    format: new ol.format.KML({
+      extractStyles:false
+    })
+  }),
+})
+
+var SPAR = new ol.layer.Tile({
+  source: new ol.source.TileWMS({
+    attributions: new ol.Attribution({
+      html: 'ESRI Maps & Data'
+    }),
+    params: {'LAYERS':'ws_ssheets:e2sfcam_blockGroups'},
+    url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
+    serverType: 'geoserver',
+    projection: projection,
+    format: new ol.format.KML({
+      extractStyles:false
+    })
+  }),
+})
+
+var SPAR = new ol.layer.Tile({
+  source: new ol.source.TileWMS({
+    attributions: new ol.Attribution({
+      html: 'ESRI Maps & Data'
+    }),
+    params: {'LAYERS':'ws_ssheets:e2sfcam_blockGroups'},
+    url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
+    serverType: 'geoserver',
+    projection: projection,
+    format: new ol.format.KML({
+      extractStyles:false
+    })
+  }),
+})
+
+var SPAR = new ol.layer.Tile({
+  source: new ol.source.TileWMS({
+    attributions: new ol.Attribution({
+      html: 'ESRI Maps & Data'
+    }),
+    params: {'LAYERS':'ws_ssheets:e2sfcam_blockGroups'},
+    url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
+    serverType: 'geoserver',
+    projection: projection,
+    format: new ol.format.KML({
+      extractStyles:false
+    })
+  }),
+})
+
+
 
 var SPAR_Map = new ol.layer.Group({
     layers: [
