0d7eab920d0f2073613d9eadd0e5bebc7be6c5f0
sagert585 <ssheets@unm.edu>
2017-05-05 12:04:18 -0600

    Update map_m13.js

--- js/map_m13.js
+++ js/map_m13.js
@@ -1,4 +1,9 @@
-var projection = ol.proj.get('EPSG:26913');
+//var projection = ol.proj.get('EPSG:26913');
+var projection = ol.proj.get('EPSG:4326');
+
+var basemap = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
 
 var SPAR = new ol.layer.Tile({
   source: new ol.source.TileWMS({
@@ -77,13 +82,13 @@ var mammograms = new ol.layer.Tile({
 
 var SPAR_Layers = new ol.layer.Group({
     layers: [
-        SPAR, cities, hospitals, mammograms
+        basemap, SPAR, cities, hospitals, mammograms
     ]
 });
 
 var SHA_Layers = new ol.layer.Group({
     layers: [
-        SHA, cities, hospitals, mammograms
+        basemap, SHA, cities, hospitals, mammograms
     ]
 });
 
@@ -91,7 +96,7 @@ var map = new ol.Map({
   target: 'map_canvas',
   view: new ol.View({
     center: ol.proj.fromLonLat([-106.583906, 35.174422]),
-    zoom: 11
+    zoom: 10
   })
 });
 
