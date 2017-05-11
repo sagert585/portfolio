92f905909223734434d9e0d0c9e27a56cb0deca2
sagert585 <ssheets@unm.edu>
2017-05-10 21:01:17 -0600

    Update final_1.js

--- js/final_1.js
+++ js/final_1.js
@@ -10,10 +10,7 @@ var tif_A5 = new ol.layer.Tile({
     params: {'LAYERS':'ws_ssheets:35106-A5'},
     url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
     serverType: 'geoserver',
-    projection: projection,
-    format: new ol.format.KML({
-      extractStyles:false
-    })
+    projection: projection
   }),
 })
 
@@ -22,21 +19,17 @@ var tif_A6 = new ol.layer.Tile({
     params: {'LAYERS':'ws_ssheets:35106-A6'},
     url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
     serverType: 'geoserver',
-    projection: projection,
-      format: new ol.format.KML({
-      extractStyles:false
-    })
+    projection: projection
   }),
 })
 
 var cont_A5 = new ol.layer.Tile({
   source: new ol.source.TileWMS({
-    params: {'LAYERS':'ws_ssheets:35106-A5_CONT'},
+    params: {'LAYERS':'ws_ssheets:35106-A5_CONT',
+              'STYLES':'cont_combined'},
     url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
     serverType: 'geoserver',
-    projection: projection,    format: new ol.format.KML({
-      extractStyles:false
-    })
+    projection: projection
   }),
 })
 
@@ -45,10 +38,7 @@ var cont_A6 = new ol.layer.Tile({
     params: {'LAYERS':'ws_ssheets:35106-A6_CONT'},
     url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
     serverType: 'geoserver',
-    projection: projection,
-    format: new ol.format.KML({
-      extractStyles:false
-    })
+    projection: projection
   }),
 })
 
@@ -57,10 +47,7 @@ var roads = new ol.layer.Tile({
     params: {'LAYERS':'ws_ssheets:tgr2006se_bern_lka'},
     url: 'http://mapper.internetmapping.net:8081/geoserver/ows?SERVICE=WMS&',
     serverType: 'geoserver',
-    projection: projection,
-    format: new ol.format.KML({
-      extractStyles:false
-    })
+    projection: projection
   }),
 });
 
